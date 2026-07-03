cb65c9b5c396b5fc188890bcac324f7b8071c1d1
princehh <136688293+pjgamer250@users.noreply.github.com>
2026-07-03 10:12:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,5 +4,5 @@ import stremlit as st
 name= st.text_input("enter your name")
 st.title("take the input")
 
-if st.button ("submit")
-st.write(f"print the name:(name)")
+if st.button ("submit"):
+st.write(f"print the name:{name}")
